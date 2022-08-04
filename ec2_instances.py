2d9d75d50de922516bda529992a4998c842d6cd2
Warren W <riess.watson18@gmail.com>
2022-08-04 08:14:13 -0500

    Created py script to start or stop instances

---
+++ ec2_instances.py
@@ -0,0 +1,19 @@
+#!/Users/wrwatso1/.pyenv/shims/python
+
+import subprocess
+from copy import copy
+import shlex
+import sys
+
+instances = subprocess.getoutput("""aws ec2 describe-instances | grep -i instanceid | cut -d ":" -f 2 | sed 's/\"//g'""").split(',')
+
+instances = [item.lstrip(' \n') for item in instances.copy()]
+
+del instances[-1]
+
+print(' '.join(instances))
+
+if sys.argv[1] == 'start':
+    subprocess.run(shlex.split(f"""aws ec2 start-instances --instance-ids {' '.join(instances)}"""))
+elif sys.argv[1] == 'stop':
+    subprocess.run(shlex.split(f"""aws ec2 stop-instances --instance-ids {' '.join(instances)}"""))
